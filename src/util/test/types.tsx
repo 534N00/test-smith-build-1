94901a38aee3161fd8e7785ab0a4cd718e573f03
GSDV <sdvgab@gmail.com>
2025-11-13 12:28:48 -0600

    Correcting true/false type

--- src/util/test/types.tsx
+++ src/util/test/types.tsx
@@ -13,6 +13,7 @@ type FreeResponseQuestion = {
 type TrueFalseQuestion = {
     type: "true-false";
     question: string;
+    answer: "true" | "false";
 };
 
 type SentenceCompletionQuuesstion = {
