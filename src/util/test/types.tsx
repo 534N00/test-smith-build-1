df3c2fdee78caf2dd9c1addcaf74bf86bd61b66e
GSDV <sdvgab@gmail.com>
2025-11-13 19:43:23 -0600

    Updating types

--- src/util/test/types.tsx
+++ src/util/test/types.tsx
@@ -1,13 +1,25 @@
+export type FreeResponseQuestion = {
+    type: "free-response";
+    question: string;
+};
+
 export type MultipleChoiceQuestion = {
     type: "multiple-choice";
     question: string;
     options: string[];
-    answers: string[];
+    answer: number; // Index of correct option.
 };
 
-export type FreeResponseQuestion = {
-    type: "free-response";
+export type SelectAllQuestion = {
+    type: "select-all";
     question: string;
+    options: string[];
+    answers: number[]; // Indices of correct options.
+};
+
+export type SentenceCompletionQuestion = {
+    type: "sentence-completion";
+    question: string; // Will include the underscores as blanks.
 };
 
 export type TrueFalseQuestion = {
@@ -16,23 +28,13 @@ export type TrueFalseQuestion = {
     answer: "true" | "false";
 };
 
-export type SentenceCompletionQuuesstion = {
-    type: "sentence-completion";
-    question: string; // Will include the underscores as blanks.
-};
 
-export type SelectionQuestion = {
-    type: "selection";
-    question: string;
-    options: string[];
-    answers: number[];
-};
-
-export type Question = MultipleChoiceQuestion
+export type Question = 
 | FreeResponseQuestion
-| TrueFalseQuestion
-| SentenceCompletionQuuesstion
-| SelectionQuestion;
+| MultipleChoiceQuestion
+| SelectAllQuestion
+| SentenceCompletionQuestion
+| TrueFalseQuestion;
 
 
 export type Test = {
